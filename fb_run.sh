1d587265ecea4c185c7d5cff3e0e2eb266ac77ec
Cassio Batista <cassio.batista.13@gmail.com>
2019-02-18 20:25:03 -0300

    fb_run script tested: up and running

---
+++ fb_run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# author: 2018
+# cassio batista - cassio.batista.13@gmail.com
+
+DT_CONTEXT=4
+DT_DATADIR="data"
+DICT_BASEFILENAME="dict_50k"
+
+# TODO: path to m2m aligner
+# get it from https://github.com/letter-to-phoneme/m2m-aligner
+M2M_ALIGNER_GITDIR="${HOME}/git-all/m2m-aligner/" 
+
+# fb_00: align
+rm -f output/${DICT_BASEFILENAME}.old.ali
+${M2M_ALIGNER_GITDIR}/m2m-aligner \
+	--delX \
+	-i input/${DICT_BASEFILENAME}.news \
+	-o output/${DICT_BASEFILENAME}.old.ali \
+	--alignerOut /tmp/nada
+
+bash ./scripts/fix_align.sh output/${DICT_BASEFILENAME}.old.ali output/${DICT_BASEFILENAME}.new.ali
+#bash ./scripts/check.sh output/${DICT_BASEFILENAME}.new.ali
+
+# fb_01: split
+python3 pysrc/split_train_test.py \
+		output/graphemes.list \
+		output/phonemes.list \
+		$DT_CONTEXT \
+		output/dict_50k.new.ali \
+		$DT_DATADIR > /dev/null
+
+# fb_02: dectree
+python3 pysrc/dectree.py \
+	output/graphemes.list \
+	output/phonemes.list \
+	$DT_CONTEXT \
+	$DT_DATADIR > /dev/null
