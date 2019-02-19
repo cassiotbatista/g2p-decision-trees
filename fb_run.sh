f12d39999190ce71b4ef5f1f86e1e0b955ec4c73
Cassio Batista <cassio.batista.13@gmail.com>
2019-02-18 22:32:56 -0300

    added script to generate list of unique features (graphemes) and labels (phonemes as classes)

--- fb_run.sh
+++ fb_run.sh
@@ -2,6 +2,7 @@
 #
 # author: 2018
 # cassio batista - cassio.batista.13@gmail.com
+# edited on feb 2019
 
 DT_CONTEXT=4
 DT_DATADIR="data"
@@ -11,7 +12,8 @@ DICT_BASEFILENAME="dict_50k"
 # get it from https://github.com/letter-to-phoneme/m2m-aligner
 M2M_ALIGNER_GITDIR="${HOME}/git-all/m2m-aligner/" 
 
-# fb_00: align
+# fb_00: align graphemes and phonemes
+mkdir -p output
 rm -f output/${DICT_BASEFILENAME}.old.ali
 ${M2M_ALIGNER_GITDIR}/m2m-aligner \
 	--delX \
@@ -19,21 +21,23 @@ ${M2M_ALIGNER_GITDIR}/m2m-aligner \
 	-o output/${DICT_BASEFILENAME}.old.ali \
 	--alignerOut /tmp/nada
 
-rm -f output/${DICT_BASEFILENAME}.ali.err
+rm -f output/${DICT_BASEFILENAME}.old.ali.err
 bash scripts/fix_align.sh output/${DICT_BASEFILENAME}.old.ali output/${DICT_BASEFILENAME}.new.ali
 #bash ./scripts/check.sh output/${DICT_BASEFILENAME}.new.ali
 
-# fb_01: split
+bash scripts/ali2instance.sh output/${DICT_BASEFILENAME}.new.ali
+
+# fb_01: split train and test
 python3 scripts/split_train_test.py \
-		output/graphemes.list \
-		output/phonemes.list \
+		res/graphemes.list \
+		res/phonemes.list \
 		$DT_CONTEXT \
 		output/dict_50k.new.ali \
 		$DT_DATADIR > /dev/null
 
-# fb_02: dectree
+# fb_02: decision tree
 python3 dt_src/dectree.py \
-	output/graphemes.list \
-	output/phonemes.list \
+	res/graphemes.list \
+	res/phonemes.list \
 	$DT_CONTEXT \
 	$DT_DATADIR > /dev/null
---
+++ scripts/ali2instance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# author: 2018
+# cassio batista - cassio.batista.13@gmail.com
+# edited on feb 2019
+
+i=1
+n=$(wc -l $1 | awk '{print $1}')
+echo "extracting list of features (graphemes) and labels (phonemes)... "
+while read line
+do
+	grapheme=$(echo $line | awk '{print $1}' | sed 's/|/ /g')
+	phonemes=$(echo $line | awk '{print $2}' | sed 's/|/ /g')
+	for letter in $grapheme ; do echo $letter >> g.tmp ; done
+	for phone in $phonemes ; do  echo $phone  >> p.tmp ; done
+	echo -ne "\r$i / $n"
+	i=$((i+1))
+done < $1
+echo
+
+echo "sorting and uniquing graphemes... "
+sort g.tmp | uniq > res/graphemes.list
+echo "sorting and uniquing phonemes... "
+sort p.tmp | uniq > res/phonemes.list
+
+rm *.tmp
